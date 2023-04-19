5619729036c05ba80e70b7b864fe43a890ffbe90
zubyr <zubyr@172.16.37.89>
2023-04-19 15:43:23 -0400

    upload full Pro-Tech project

---
+++ Pro_Tech/src/Package2/Class2.java
@@ -0,0 +1,71 @@
+package Package2;
+
+public class Class2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("This is my Second Program");
+		
+		
+		
+	int a = 50;
+	int b = 10;
+	
+	int addition = a + b;
+	int subraction = a - b;
+	int multiplication = a * b;
+	float division = a / b;
+	
+	System.out.println("Result of a Plus b = " + addition);
+	System.out.println("Result of a Minus b = " + subraction);
+	System.out.println("Result of a multiply b = " +  multiplication);
+	System.out.println("Result of a dividby b = " + division);
+	
+	/*har a = 'C';
+	
+	boolean a = true;
+	boolean b = false;
+	//System.out.println(c);
+	 */
+	 
+	
+	
+	int drivingAge = 18;
+	if (drivingAge >= 18){
+		System.out.println("eligible to drive");
+	}
+	else {
+		System.out.println("Not eligible to drive");
+	}
+		
+		
+		
+	int percentage = 50;
+	
+if (percentage >= 80 && percentage <=100) {
+	System.out.println("Grade A");
+}
+else if (percentage >= 70 && percentage <= 79) {
+	System.out.println("Grade B");
+}
+else if (percentage >= 50 && percentage <= 69) {
+	System.out.println("Grade C");
+}
+else {
+	System.out.println("Fail");
+}
+	
+	
+			
+			
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
